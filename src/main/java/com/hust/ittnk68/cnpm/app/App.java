94440148bc5fabd8b568275bd55630c774d1c596
Nguyen Dinh Dang Duong <dangduong31205@gmail.com>
2025-03-11 23:03:15 +0700

    code for test springboot

--- src/main/java/com/hust/ittnk68/cnpm/app/App.java
+++ src/main/java/com/hust/ittnk68/cnpm/app/App.java
@@ -11,6 +11,10 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.NoSuchFileException;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriBuilder;
+
 import com.hust.ittnk68.cnpm.config.MySQLDefaultConfig;
 import com.hust.ittnk68.cnpm.database.GetSQLProperties;
 import com.hust.ittnk68.cnpm.database.MySQLDatabase;
@@ -33,46 +37,15 @@ import com.hust.ittnk68.cnpm.model.Family;
 // import javafx.geometry.*;
 // import javafx.animation.*;
 
+import com.hust.ittnk68.cnpm.communication.*;
+
 public class App {
 	public static void main(String[] args) throws NoSuchFileException, IOException, ConfigFileException {
-
-		// Person p1 = new Person(69, "ND Dang Duong", new Date(2005, 12, 3), "010039931183", "0912375999", Sex.MALE, Nation.VIETNAM, ResidenceStatus.PRESENT);
-		// Person p2 = new Person(69, "Phuc Du", new Date(1997, 3, 06), "010039931183", "0912375999", Sex.MALE, Nation.USA, ResidenceStatus.ABSENT);
-		// Person p3 = new Person(69, "Ablaham", new Date(1977, 9, 15), "010039931183", "0912375999", Sex.FEMALE, Nation.INDIA, ResidenceStatus.PRESENT);
-		// Person p4 = new Person(1, "Ablaham", new Date(1977, 9, 15), "010039931183", "0912375999", Sex.FEMALE, Nation.INDIA, ResidenceStatus.PRESENT);
-		// Person p5 = new Person(2, "Ablaham", new Date(1977, 9, 15), "010039931183", "0912375999", Sex.FEMALE, Nation.INDIA, ResidenceStatus.PRESENT);
-		// Person p6 = new Person(33, "Ablaham", new Date(1977, 9, 15), "010039931183", "0912375999", Sex.FEMALE, Nation.INDIA, ResidenceStatus.PRESENT);
-		//
-		//
-		// try {
-		// 	sql.create(p1);
-		// 	sql.create(p2);
-		// 	sql.create(p3);
-		// 	sql.create(p4);
-		// 	sql.create(p5);
-		// 	sql.create(p6);
-		// }
-		// catch (Exception e) {
-		// 	e.printStackTrace();
-		// }
-		//
-		// Family f = new Family(2, "A35");
-		//
-		// try {
-		// 	sql.create(f);
-		// }
-		// catch (Exception e) {
-		// 	e.printStackTrace();
-		// }
-		//
-		// Account acc = new Account(1, "root", "tomoshibi");
-		//
-		// try {
-		// 	int id = sql.create(acc);
-		// 	System.out.printf("insert id = %d\n", id);
-		// }
-		// catch (Exception e) {
-		// 	e.printStackTrace();
-		// }
-	} 
+		RestClient restClient = RestClient.create ();
+		ServerResponseStartSession res = restClient.post()
+			.uri ("http://127.0.0.1:8080" + ApiMapping.START_SESSION)
+			.body (new ClientMessageStartSession ("abcd", "efgh"))
+			.retrieve ()
+			.body (ServerResponseStartSession.class);
+	}
 }

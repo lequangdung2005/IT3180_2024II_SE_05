271c71141471ce26b43f849e81cc35c9afa6ebc9
Nguyen Dinh Dang Duong <dangduong31205@gmail.com>
2025-03-27 15:54:12 +0700

    style progress-bar

--- src/main/java/com/hust/ittnk68/cnpm/view/LoadingView.java
+++ src/main/java/com/hust/ittnk68/cnpm/view/LoadingView.java
@@ -1,6 +1,7 @@
 package com.hust.ittnk68.cnpm.view;
 
 import javafx.concurrent.Task;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.VBox;
@@ -13,7 +14,7 @@ public class LoadingView extends VBox {
     public <T> LoadingView(Task<T> task) {
         ProgressBar pBar = new ProgressBar();
         pBar.progressProperty().bind(task.progressProperty());
-        pBar.setPrefSize (200, 24);
+        pBar.setPrefSize (300, 24);
 
         Label statusLabel = new Label();
         statusLabel.textProperty().bind(task.messageProperty());
@@ -22,12 +23,17 @@ public class LoadingView extends VBox {
         this.statusLabel = statusLabel;
 
         this.setSpacing (5);
+        this.setPadding (new Insets (16));
         this.getChildren ().addAll (pBar, statusLabel);
 
-        this.getStylesheets().add (
+        this.getStylesheets().addAll (
             getClass().getResource (
                 "/css/progress-bar.css"
             ).toExternalForm()
+        );
+
+        this.getStyleClass ().addAll (
+            "graywrapper", "loadingview"
         );
     }
 }

86a9eb54469e96700b276d7ee5ba3a55a7e7321b
Nguyen Dinh Dang Duong <dangduong31205@gmail.com>
2025-05-09 14:31:41 +0700

    add tests

--- src/test/java/com/hust/ittnk68/cnpm/app/AppTest.java
+++ src/test/java/com/hust/ittnk68/cnpm/app/AppTest.java
@@ -4,35 +4,55 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+import java.security.*;
+
+import io.jsonwebtoken.*;
+
+import org.springframework.context.annotation.*;
+
+import com.hust.ittnk68.cnpm.model.*;
+import com.hust.ittnk68.cnpm.type.*;
+import com.hust.ittnk68.cnpm.auth.*;
+import com.hust.ittnk68.cnpm.security.*;
+
+public class AppTest extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
+
     public AppTest( String testName )
     {
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
+    public void testSign ()
     {
-        assertTrue( true );
+        RsaKeyUtil rsaKeyUtil = new RsaKeyUtil ();
+        KeyPair keyPair = rsaKeyUtil.generateKeyPair ();
+        PrivateKey privateKey = keyPair.getPrivate ();
+        String token = Jwts.builder()
+            .setSubject ("xyz")
+            .signWith (privateKey, SignatureAlgorithm.RS256)
+            .compact ();
+        assertTrue (token != null);
+    }
+
+    public void testAuth () throws Exception
+    {
+        Account acc = new Account (59, "anbatocom", "dfjsdl39fjs???jdlfjslfloewjlkjlf", AccountType.USER);
+        JwtUtil jwtUtil = new JwtUtil ();
+
+        String token = jwtUtil.generateToken (acc);
+
+        System.out.printf ("token = %s\n", token);
+
+        Account tmp = jwtUtil.extractAccount (token);
+        assertTrue (tmp.getFamilyId () == 59);
+        assertTrue (tmp.getUsername().equals("anbatocom"));
+        assertTrue (tmp.getDigestPassword().equals("dfjsdl39fjs???jdlfjslfloewjlkjlf"));
+        assertTrue (tmp.getAccountType().equals(AccountType.USER));
     }
 }
